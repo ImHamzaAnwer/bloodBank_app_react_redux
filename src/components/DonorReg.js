593a9d89b9068ab778046123467c6db2bc9059cf
imHamzaAnwer <hamza.anwer8@gmail.com>
2017-04-10 02:13:03 +0500

    changed state to true

---
+++ src/components/DonorReg.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import DonorRegCont from '../containers/DonorRegContainer';
+
+class DonorReg extends Component{
+    render(){
+        return (
+            <DonorRegCont/>
+        );
+    }
+}
+
+export default DonorReg;
